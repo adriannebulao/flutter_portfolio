9da25dd08fd5b50aa78481b33cdd756ad613574d
adriannebulao <amcbulao@gmail.com>
2024-10-15 22:15:08 +0800

    feat: draft layout of main page

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -6,9 +6,52 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: Center(
-        child: Text("Hello"),
-      ),
-    );
+        body: Column(
+      children: [
+        Row(
+          children: [Text("Top Bar")],
+        ),
+        Column(
+          children: [
+            Text("Hi, I am Adrianne Mykhel Bulao"),
+            Text("A Back End Engineer."),
+            Text("description"),
+            Row(
+              children: [
+                Text("resume"),
+                SizedBox(
+                  width: 25,
+                ),
+                Text("github"),
+                SizedBox(
+                  width: 25,
+                ),
+                Text("linkedin")
+              ],
+            )
+          ],
+        ),
+        Column(
+          children: [
+            Text("Projects"),
+            Row(
+              children: [Text("Project 1"), Text("Proejct 2"), Text("Text 3")],
+            )
+          ],
+        ),
+        Column(
+          children: [
+            Text("Skills"),
+            Row(
+              children: [Text("HTML"), Text("CSS"), Text("JavaScript")],
+            )
+          ],
+        ),
+        Column(
+          children: [Text("Contact"), Text("email me")],
+        ),
+        Text("Created By Adrianne Bulao")
+      ],
+    ));
   }
 }
